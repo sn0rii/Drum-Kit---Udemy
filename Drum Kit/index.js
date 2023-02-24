8d65fe23b4d801bccaedc6718ea2f99c2bcfd8c9
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-02-24 08:20:58 +0100

    update

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -5,11 +5,17 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     var buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
   });
 }
 
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+
+ buttonAnimation(event.key)
+
 });
 
 function makeSound(key) {
@@ -46,3 +52,17 @@ function makeSound(key) {
     default:
   }
 }
+
+
+
+function buttonAnimation(currentKey){
+
+var activeButton = document.querySelector("." + currentKey)
+
+activeButton.classList.add("pressed");
+
+setTimeout(function() {
+  activeButton.classList.remove("pressed");
+}, 100)
+
+};
